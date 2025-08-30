e37e004f0a7bb55628bd2ea9c69ac336ca8e5480
chetandasgondaliya-lgtm <chetandasgondaliya@gmail.com>
2025-08-30 20:21:53 +0530

    Add files via upload

---
+++ m1 task2.py
@@ -0,0 +1,6 @@
+
+first_name = input("enter your name: ")
+last_name = input("enter your last name: ")
+full_name = first_name +" "+ last_name
+
+print("hello, " + full_name + " welcome to python program.")
